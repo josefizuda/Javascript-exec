81152087fb28cbb46cad0448111f0d86dbaa7b6b
Josef Weslley Batista dos santos <30638789+josefizuda@users.noreply.github.com>
2021-10-26 10:34:05 -0300

    Criação de pasta

---
+++ exec1/main.js
@@ -0,0 +1,11 @@
+const nomeCompleto = "Josef Weslley batista dos santos"; // String
+let cidade = "Blumenau"; // String
+let empresa = "Senior Sistemas"; // String
+let peso = 70.75; // Number
+let estudaProgramacao = true; // Boolean
+let idade = 26; // Number
+let altura; // Undefined
+let corRaca = null; // Redefinir Valor
+
+
+console.log(nomeCompleto);
